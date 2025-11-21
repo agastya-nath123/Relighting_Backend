6899c8b147c4ca48bf50a8725ac26f24ff83ef37
Agastya Nath <agastyanath1@gmail.com>
2025-11-21 18:48:00 +0530

    Second rites

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+import torch
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "hello, your API didn't explode"}
+    mes1 = torch.backends.mps.is_available()
+    mes2 = torch.backends.mps.is_built()
+    return {"message1": mes1, "message2": mes2}
 
